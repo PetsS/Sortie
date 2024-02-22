10d8af2e24c14fd1b51f24848b66c25e6addee28
MarieEchard <marie.echard79@gmail.com>
2024-02-22 17:00:31 +0100

    création début page liste sortie affichage informations incrémentation des inscrits apparition des boutons création fonction s'inscrire, se désinscrire
    reste à: régler l'inscription (reste sur False)

--- src/Controller/SortieController.php
+++ src/Controller/SortieController.php
@@ -2,6 +2,10 @@
 
 namespace App\Controller;
 
+use App\Entity\Sortie;
+use App\Entity\User;
+use App\Repository\SortieRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
@@ -10,8 +14,63 @@ use Symfony\Component\Routing\Attribute\Route;
 class SortieController extends AbstractController
 {
     #[Route('/liste', name: '_liste')]
-    public function listeSortie(): Response
+    public function listeSortie(SortieRepository $sortieRepository): Response
     {
-        return $this->render('sortie/listeSortie.html.twig');
+        $userId = $this->getUser();
+        $sorties = $sortieRepository->findAll();
+        $isUserInscrit = false;
+        foreach ($sorties as $sortie ){
+            foreach ($sortie ->getParticipants() as $participant) {
+                if ($participant -> getId() === $userId){
+                    $isUserInscrit = true;
+                }
+            }
+        }
+        var_dump( $isUserInscrit);
+        return $this->render('sortie/listeSortie.html.twig', [
+            'sorties' => $sorties,
+            'isUserInscrit' => $isUserInscrit
+        ]);
+    }
+
+    #[Route('/detail/{id}', name: '_detail', requirements: ['id' => '\d+'])]
+    public function detail( int $id): Response
+    {
+        return $this->render('sortie/infoSortie.html.twig', [
+            'id' => $id
+        ]);
+    }
+
+    #[Route('/inscription/{sortie}', name: '_inscription', requirements: ['sortie' => '\d+'])]
+    public function inscription(Sortie $sortie, EntityManagerInterface $em): Response
+    {
+        $user = $this->getUser();
+
+        if ($user instanceof \App\Entity\User){
+            $sortie-> addParticipant($user);
+        }
+
+        $em->persist($user);
+        $em->flush();
+
+        $this->addFlash('success', 'Vous avez été ajouté à la liste des participants !');
+        return $this->redirectToRoute('app_sortie_liste');
+    }
+
+    #[Route('/desinscription/{sortie}', name: '_desinscription', requirements: ['sortie' => '\d+'])]
+    public function desinscription(Sortie $sortie, EntityManagerInterface $em): Response
+    {
+
+        $user = $this->getUser();
+
+        if ($user instanceof \App\Entity\User){
+            $sortie-> removeParticipant($user);
+        }
+
+        $em->persist($user);
+        $em->flush();
+
+        $this->addFlash('success', 'Vous avez été retiré de la liste des participants !');
+        return $this->redirectToRoute('app_sortie_liste');
     }
 }
