652b8d574910b27d0af69a364d36a0cc8f28ad9b
aavilannova2023 <avilan.alexandra.f@gmail.com>
2024-02-23 17:02:54 +0100

    creation sortie

--- src/Controller/SortieController.php
+++ src/Controller/SortieController.php
@@ -4,7 +4,6 @@ namespace App\Controller;
 
 use App\Entity\Sortie;
 use App\Form\CreerUneSortieType;
-use App\Repository\AdresseRepository;
 use App\Repository\SortieRepository;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
@@ -23,40 +22,37 @@ class SortieController extends AbstractController
     }
 
     #[Route('/detail/{id}', name: '_detail',requirements: ['id' =>'\d+'])]
-    public function Detaildelasortie(int $id, SortieRepository $sortieRepository, AdresseRepository $adresseRepository): Response
+    public function Detaildelasortie(int $id, SortieRepository $sortieRepository): Response
     {
         $sortie = $sortieRepository->find($id);
+
         return $this->render('sortie_detail/sortie_detail.html.twig', [
             'sortie' => $sortie
+
         ]);
     }
 
        #[Route('/create', name: '_create')]
-    public function create(Request $request, EntityManagerInterface $em, SluggerInterface $slugger): Response
+    public function create(Request $request, EntityManagerInterface $em): Response
 
     {
-
         $sortie = new Sortie();
 
         $form = $this->createForm(CreerUneSortieType::class, $sortie);
 
-
-
         $form->handleRequest($request);
 
         if ($form->isSubmitted() && $form->isValid()) {
-            $em= $this->getDoctrine()->getManager();
             $em->persist($sortie);
-
             $em->flush();
 
-            // $this->addFlash('success', 'La série a été enregistrée');
+             $this->addFlash('success', 'La sortie a été enregistrée');
 
             return $this->redirectToRoute('app_sortie_liste');
 
         }
 
-        return $this->render('sortie_detail/sortie_detail.html.twig', [
+        return $this->render('sortie_detail/edit.html.twig', [
 
             'form' => $form
 
