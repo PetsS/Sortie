baf61e654136eb34cb3fc050542df4093e73ded6
aavilannova2023 <avilan.alexandra.f@gmail.com>
2024-02-23 17:07:48 +0100

    creation sortie

---
+++ src/Form/AdresseType.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Adresse;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class AdresseType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('nomLieu')
+            ->add('rue')
+            ->add('codePostal')
+            ->add('ville')
+            ->add('numeroRue')
+            ->add('latitude')
+            ->add('longitude')
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Adresse::class,
+        ]);
+    }
+}
