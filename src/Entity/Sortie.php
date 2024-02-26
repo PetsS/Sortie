347d5c538205cfb054390ffe9d6228f46e290c02
mmahuas2023 <melissa.mahuas@gmail.fr>
2024-02-26 16:57:45 +0100

    etat

--- src/Entity/Sortie.php
+++ src/Entity/Sortie.php
@@ -9,6 +9,7 @@ use Doctrine\DBAL\Types\Types;
 use Doctrine\ORM\Mapping as ORM;
 
 #[ORM\Entity(repositoryClass: SortieRepository::class)]
+#[ORM\EntityListeners([EtatListener::class])]
 #[ORM\HasLifecycleCallbacks]
 class Sortie
 {
