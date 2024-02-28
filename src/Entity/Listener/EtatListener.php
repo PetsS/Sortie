41bf30bcd0ae410f7a19a209c7345b8025ad5781
MarieEchard <marie.echard79@gmail.com>
2024-02-28 14:35:21 +0100

    suppression etatListener innutile + modification du 2ème pour les états

--- src/Entity/Listener/EtatListener.php
+++ src/Entity/Listener/EtatListener.php
@@ -1,21 +1,39 @@
 <?php
 
 namespace App\Entity\Listener;
-
 use App\Entity\Sortie;
 
 class EtatListener
 {
-    public function postLoad(Sortie $sortie): void
+
+    public function postLoad(): void
     {
-     if($sortie->getDateLimiteInscription() < new \DateTime()) {
-         $sortie->setEtat('TERMINER');
-     }
+        $sortie = getSortie();
+        $dateNow = new \DateTime();
+        $dureeMinutes = $sortie->getDuree();
+        $dureeSecondes = $dureeMinutes*60;
+        $dureeTimeStamp = time()+$dureeSecondes;
 
+        $conversionDuree = \DateTime::createFromFormat('U', $dureeTimeStamp);
 
+        $debut = $sortie->getDateDebut();
 
+        dd($debut);
+        $conversionDuree->setTimestamp($debut->getTimestamp());
+
+        if ($sortie->getDateLimiteInscription() >= $dateNow) {
+            $sortie->setEtat('OUVERT');
+        }elseif(($sortie->getDateLimiteInscription() < $dateNow) && ($dateNow < $sortie->getDateDebut())){
+            $sortie->setEtat('FERME');
+//        } elseif (($sortie->getDateDebut() <= $dateNow) && ($dateNow < $conversionDuree)){
+        } elseif ($dateNow > $sortie->getDateDebut()){
+            $sortie->setEtat('EN COURS');
+        } elseif ($conversionDuree <= $dateNow){
+            $sortie->setEtat('TERMINE');
+        }
     }
 
-
-
+    public function prePersist(Sortie $sortie) {
+        $sortie->setEtat('EN COURS');
+    }
 }
